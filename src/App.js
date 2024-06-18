15c34c439cf62080127c3de733cf7134fccfefb0
swapnilranmale2003 <swapnilranmale2003@gmail.com>
2024-06-18 18:28:46 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,43 @@
-import logo from './logo.svg';
-import './App.css';
 
+import { getFirestore, collection, addDoc, doc, getDoc,  query, where, getDocs } from "firebase/firestore";
+import {app} from './firebase'
+const firestore = getFirestore(app);
 function App() {
+  const writeData = async()=>{
+   const result = await addDoc(collection(firestore,'cities'),{
+      name:"Nashik",
+      pinCode:422010
+    })
+    console.log("Result",result);
+  }
+  const writeSubData = ()=>{
+    addDoc(collection(firestore,'cities/3lPjf3fZ14bXELw2qZGR/places'),{
+      name:"this is place",
+      des:"awsm place",
+      date:Date.now()
+    });
+  }
+  const getData = async()=>{
+    const docRef = doc(firestore, "cities","N49XlttFySv1EbxQXywL");
+    const docSnap = await getDoc(docRef);
+    console.log(docSnap.data());
+  }
+  const getDocument = async ()=>{
+      const qref = collection(firestore,'users');
+      const q =  query(qref, where('isMale', '==', false));
+      const snap=await getDocs(q);
+      snap.forEach((data)=>console.log(data.data()))
+  }
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+     <h1>Firebase cloudstore</h1>
+     <button onClick={writeData}>PutData</button>
+     <button onClick={writeSubData}>PutData</button>
+     <button onClick={getData}>GetData</button>
+<button onClick={getDocument}>getDocument</button>
+
     </div>
+
   );
 }
 
---
+++ src/firebase.js
@@ -0,0 +1,13 @@
+import { initializeApp } from "firebase/app";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyBREZXc_dYgdLDlirASYvnVI2XU8itc3Is",
+  authDomain: "firestore-learning-1325c.firebaseapp.com",
+  projectId: "firestore-learning-1325c",
+  storageBucket: "firestore-learning-1325c.appspot.com",
+  messagingSenderId: "746307569196",
+  appId: "1:746307569196:web:863eefba52ef433275f389",
+  measurementId: "G-NGS202TRRC"
+};
+
+export const app = initializeApp(firebaseConfig);
